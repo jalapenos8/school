bf3d143419835a7c4afe1051935500309491500f
Nurmukhamed <nur.orazbek005@gmail.com>
2023-11-16 14:54:35 +0100

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,30 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/**.js',
-                'resources/css/**.css',
             ],
             refresh: true,
         }),
     ],
+    build: {
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'resources/js/[name]-[hash].js',
+          entryFileNames: 'resources/js/[name]-[hash].js',
+          
+          assetFileNames: ({name}) => {         
+            if (/\.js$/.test(name ?? '')) {
+                return 'resources/js/[name]-[hash][extname]';   
+            }
+
+            if (/\.css$/.test(name ?? '')) {
+                return 'resources/css/[name]-[hash][extname]';   
+            }
+   
+            // default value
+            // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+            return 'resources/[name]-[hash][extname]';
+          },
+        },
+      }
+    },
 });
