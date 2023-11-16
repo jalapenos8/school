4b5fdb54768b7adc0f7bf486fefd16c825dfc97c
Nurmukhamed <nur.orazbek005@gmail.com>
2023-11-16 14:51:47 +0100

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/createquiz.js',
+                'resources/js/**.js',
+                'resources/css/**.css',
             ],
             refresh: true,
         }),
