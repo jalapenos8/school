5e38ad019186069172625616723f5b3aa552e2fa
Nurmukhamed <nur.orazbek005@gmail.com>
2023-11-25 17:32:00 +0100

    steps.show lesson-content height and buttons position fixed

--- resources/views/components/lesson-content.blade.php
+++ resources/views/components/lesson-content.blade.php
@@ -1,3 +1,3 @@
-<div id="lessonContent" {{ $attributes->merge(['class' => 'flex overflow-y-auto flex-col ml-2 py-6 px-10 rounded-lg bg-white dark:bg-gray-800', 'style'=>'width:75%; height: 80vh;'])}}> {{-- lessons and activities --}}
+<div id="lessonContent" {{ $attributes->merge(['class' => 'flex overflow-y-auto flex-col ml-2 px-10 rounded-lg bg-white dark:bg-gray-800', 'style'=>'width:75%; height: calc(100vh - 190px);'])}}> {{-- lessons and activities --}}
     {{ $slot }}
 </div>
--- resources/views/steps/show.blade.php
+++ resources/views/steps/show.blade.php
@@ -11,46 +11,48 @@
 
     {{ Breadcrumbs::render('steps.show', $subject, $unit, $lesson, $step) }}
 
-    <div class="flex max-w-7xl pb-4 mx-auto mt-2 px-4 sm:px-6 lg:px-8 text-lg text-gray-800 dark:text-gray-200 leading-tight">
+    <div class="flex max-w-7xl pb-2 mx-auto mt-2 px-4 sm:px-6 lg:px-8 text-lg text-gray-800 dark:text-gray-200 leading-tight">
         @include('components.side-bar')
         
         <x-lesson-content class="text-xl leading-9">
-            <div class="flex items-center w-full">
-                <span class="text-2xl">{{ $step->name }}</span>
+            <div class="flex flex-col min-h-full">
+                <div class="flex items-center w-full grow-0 shrink basis-auto pt-6">
+                    <span class="text-2xl">{{ $step->name }}</span>
 
-                @if ($step->users->contains(Auth::user()->id))
-                    <i class="pl-1 fa fa-check text-green-500 px-2" aria-hidden="true"></i> 
-                @endif
+                    @if ($step->users->contains(Auth::user()->id))
+                        <i class="pl-1 fa fa-check text-green-500 px-2" aria-hidden="true"></i> 
+                    @endif
+
+                    @subject($subject)
+                        <a class="ml-6" href="{{ route('steps.edit', [$subject, $unit, $lesson, $step]) }}"><x-secondary-button>Edit</x-secondary-button></a>
+                        <a class="ml-2" href="{{ route('steps.delete', [$subject, $unit, $lesson, $step]) }}"><x-danger-button>Delete</x-danger-button></a>
+                    @endsubject
+                </div>
+                <div class="flex grow-1 shrink basis-auto">
+                    <div class="w-full">
+                        @if($step->type == 'theory')
+                            <?php print_r($step->content); ?>
+                        @elseif($step->type == 'file')
+                            <a href="{{ asset('storage/'.$step->content)}}">Скачать файл</a>
+                        @elseif($step->type == 'video')
+                            <?php
+                                $parts = parse_url($step->content);
+                                parse_str($parts['query'], $query);
+                            ?>
+                            <iframe class="mx-auto" width="90%" height="90%" modestbranding rel=0 showinfo=0 controls=0 src="https://www.youtube.com/embed/{{ $query['v'] }}?rel=0&showinfo=0"  allowfullscreen></iframe>
+                        @elseif($step->type == 'interactive')
+                            <?php
+                                $playId = explode("play", $step->content);
+                            ?>
+                            <iframe width="100%" height="100%" class="mx-auto" src="https://wordwall.net/embed/play{{ $playId[1] }}" frameborder="0" allowfullscreen></iframe>
                     
-                @subject($subject)
-                    <a class="ml-6" href="{{ route('steps.edit', [$subject, $unit, $lesson, $step]) }}"><x-secondary-button>Edit</x-secondary-button></a>
-                    <a class="ml-2" href="{{ route('steps.delete', [$subject, $unit, $lesson, $step]) }}"><x-danger-button>Delete</x-danger-button></a>
-                @endsubject
-            </div>
-            <div class="w-full h-full mt-4">
-                @if($step->type == 'theory')
-                    <?php print_r($step->content); ?>
-                @elseif($step->type == 'file')
-                    <a href="{{ asset('storage/'.$step->content)}}">Скачать файл</a>
-                @elseif($step->type == 'video')
-                    <?php
-                        $parts = parse_url($step->content);
-                        parse_str($parts['query'], $query);
-                    ?>
-                    <iframe class="mx-auto" width="90%" height="90%" modestbranding rel=0 showinfo=0 controls=0 src="https://www.youtube.com/embed/{{ $query['v'] }}?rel=0&showinfo=0"  allowfullscreen></iframe>
-                @elseif($step->type == 'interactive')
-                    <?php
-                        $playId = explode("play", $step->content);
-                    ?>
-                    <iframe width="100%" height="100%" class="mx-auto" src="https://wordwall.net/embed/play{{ $playId[1] }}" frameborder="0" allowfullscreen></iframe>
-            
-                    </form>
-                @elseif($step->type == 'whiteboard')
-                
-                @endif
-            </div>
-            <div>
-                <div class="flex items-center justify-between">
+                            </form>
+                        @elseif($step->type == 'whiteboard')
+                        
+                        @endif
+                    </div>
+                </div>
+                <div class="flex items-center justify-between grow-0 shrink mt-auto pb-6">
                     <form method="POST" action="{{ route('steps.submit', [$subject, $unit, $lesson, $step]) }}">
                         @csrf
                         <x-primary-button>
