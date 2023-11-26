f9e9f6de3ec39c19a6977e7a2a51f557a62f9e80
Nurmukhamed <nur.orazbek005@gmail.com>
2023-11-26 22:07:49 +0100

    favicon of NIS added

--- resources/views/layouts/quiz.blade.php
+++ resources/views/layouts/quiz.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="icon" type="image/x-icon" href="https://kzl.nis.edu.kz/wp-content/uploads/2017/02/logo_black.png">
         <meta http-equiv="pragma" content="no-cache">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
